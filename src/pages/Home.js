cda45094da852ee46e24a3a77147c09eb0142f70
Liam Townsley <contact@liamtownsley.me>
2022-06-06 18:09:34 +0100

    feat: adding firestore

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,26 @@
 import { Carousel } from '../components';
 import styled from 'styled-components';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDXQ5ML8SqOT5ng4anDIuWK5nnHnFluY5Y",
+  authDomain: "note-it-9c761.firebaseapp.com",
+  projectId: "note-it-9c761",
+  storageBucket: "note-it-9c761.appspot.com",
+  messagingSenderId: "102619486473",
+  appId: "1:102619486473:web:a52f63b0ac95f148d9dbd8",
+  measurementId: "G-NBVC7RS9VT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 const PageParent = styled.div`
   padding: 20px;
